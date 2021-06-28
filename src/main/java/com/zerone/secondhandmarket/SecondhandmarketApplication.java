a72c91b7292ffa28f89d5764928237e59a7fbc29
jxy <858297426@qq.com>
2021-06-28 17:17:58 +0800

    Initial commit

---
+++ src/main/java/com/zerone/secondhandmarket/SecondhandmarketApplication.java
@@ -0,0 +1,13 @@
+package com.zerone.secondhandmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandmarketApplication.class, args);
+    }
+
+}
