3c0efeb5cae000c58f28caf3a60e1a019803fcc1
Pecco <pecco@qq.com>
2021-06-29 09:45:53 +0800

    使得项目可以解析html

--- src/main/java/com/zerone/secondhandmarket/SecondHandMarketController.java
+++ src/main/java/com/zerone/secondhandmarket/SecondHandMarketController.java
@@ -1,7 +1,7 @@
 package com.zerone.secondhandmarket;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class SecondHandMarketController {
