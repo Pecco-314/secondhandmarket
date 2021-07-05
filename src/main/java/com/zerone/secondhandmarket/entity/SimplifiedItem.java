570883da5ed1f948df50e2cc3a5f90d884c2af31
Admin <c170325225@163.com>
2021-07-05 15:39:05 +0800

    添加了SimplifiedItem

--- src/main/java/com/zerone/secondhandmarket/entity/SimplifiedItem.java
+++ src/main/java/com/zerone/secondhandmarket/entity/SimplifiedItem.java
@@ -1,6 +1,5 @@
 package com.zerone.secondhandmarket.entity;
 
-import com.zerone.secondhandmarket.enums.ItemType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor  //无参构造函数
 public class SimplifiedItem {
     private int item_id;//物品的id
-    private int seller_id;//卖家的id
     private String item_name;//物品名称
-    private ItemType type;//物品类型
-    private int quantity;//数量
     private double price_now;//现在的价格
-    private double price_original;//原来的价格
-    private String keyword;//关键词
-    private String introduction;//简介
+    private String[] keyword;//关键词
     private String item_pic_path;//上传的商品的图片的路径，可能有多张图片存在一个文件夹里，即文件夹路径
 }
