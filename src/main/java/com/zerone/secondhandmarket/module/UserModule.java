1074fc7a37ebb2c5702b2e76b2fe907b3c987365
Admin <c170325225@163.com>
2021-07-06 10:56:02 +0800

    写了一些模块中的方法

--- src/main/java/com/zerone/secondhandmarket/module/UserModule.java
+++ src/main/java/com/zerone/secondhandmarket/module/UserModule.java
@@ -8,16 +8,29 @@ import com.zerone.secondhandmarket.enums.Status;
 public class UserModule {
     public static Result getUserInfo(UserService userService, int userid) {
         User user = userService.getUserById(userid);
+
+        if(user == null)
+            return new Result(Status.ERROR, "", null);
+
         return new Result(Status.OK, "获取成功", user);
     }
 
     public static Result updateUserInfo(UserService userService, User user) {
-        userService.updateUser(user);
-        return new Result(Status.OK, "更新成功", null);
+        try {
+            userService.updateUser(user);
+
+            return new Result(Status.OK, "更新成功", null);
+        } catch (Exception e) {
+            return new Result(Status.ERROR, "", null);
+        }
     }
 
     public static Result deleteUserInfo(UserService userService, int userid) {
-        userService.deleteUser(userid);
-        return new Result(Status.OK, "删除成功", null);
+        try {
+            userService.deleteUser(userid);
+            return new Result(Status.OK, "删除成功", null);
+        } catch (Exception e) {
+            return new Result(Status.ERROR, "", null);
+        }
     }
 }
