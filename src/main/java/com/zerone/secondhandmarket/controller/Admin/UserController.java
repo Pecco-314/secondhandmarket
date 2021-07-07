69988b165e5d8114088da14ed61518f9a11413d7
lzl <1042173993@qq.com>
2021-07-07 22:25:40 +0800

    Controller与Module更新

--- src/main/java/com/zerone/secondhandmarket/controller/Admin/UserController.java
+++ src/main/java/com/zerone/secondhandmarket/controller/Admin/UserController.java
@@ -4,9 +4,8 @@ import com.zerone.secondhandmarket.entity.User;
 import com.zerone.secondhandmarket.message.UserModificationByAdministratorMessage;
 import com.zerone.secondhandmarket.module.UserModule;
 import com.zerone.secondhandmarket.service.UserService;
-import com.zerone.secondhandmarket.tools.JSONMapper;
 import com.zerone.secondhandmarket.viewobject.Result;
-import netscape.javascript.JSObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,31 +14,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("AdminUser")
 public class UserController {
-    private UserService userService = new UserService();
-
-    public String getUserList() {
-        Result result = UserModule.getUserList(userService);
-
-        return JSONMapper.writeValueAsString(result);
-    }
+    @Autowired
+    private UserService userService=new UserService();
 
     @GetMapping("/requests/delete/{userId}")
     @ResponseBody
     public String deleteUser(@PathVariable int userId){
-        Result result = UserModule.deleteUser(userService, userId);
+        Result result= UserModule.deleteUser(userService,userId);
 
-        return JSONMapper.writeValueAsString(result);
+        return result.toString();
     }
 
     @GetMapping("/requests/modify")
     @ResponseBody
     public String modifyUser(@RequestBody UserModificationByAdministratorMessage userModificationByAdministratorMessage){
         //获取id对应的当前用户信息
-        User user = userService.getUserById(userModificationByAdministratorMessage.getUserID());
+        User user=userService.getUserById(userModificationByAdministratorMessage.getUserID());
         //设置更新后的用户信息
         user.setNickname(userModificationByAdministratorMessage.getNickName());
         Result result= UserModule.updateUserInfo(userService,user);
 
-        return JSONMapper.writeValueAsString(result);
+        return result.toString();
     }
 }
