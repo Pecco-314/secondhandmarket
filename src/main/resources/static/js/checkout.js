57588749b32024df84abe95a7bef357600d56e76
Pecco <pecco@qq.com>
2021-07-13 12:01:57 +0800

    完成订单页面功能

--- src/main/resources/static/js/checkout.js
+++ src/main/resources/static/js/checkout.js
@@ -30,6 +30,7 @@ Vue.component('campus-select', {
     },
 })
 
+
 let checkoutForm = new Vue({
     el: "#checkout-form",
     methods: {
@@ -64,12 +65,130 @@ let checkoutForm = new Vue({
                 }
             }],
             dorm: [{required: true, message: "请输入公寓园区"}],
-        }
+        },
+        id: getURLVariable('id')
     },
 });
+
+Vue.component('checkout-item', {
+    props: ["itemId"],
+    methods: {
+        getData(id) {
+            getItemInfo(id, response => {
+                this.imagePath = response.data.itemImages[0];
+                this.name = response.data.name;
+                this.price = response.data.price;
+                checkoutConfirm.updateTotalPrice();
+            })
+        }
+    },
+    mounted() {
+        this.getData(this.itemId);
+    },
+    data() {
+        return {
+            imagePath: '',
+            name: '',
+            price: 0,
+            quantity: 1,
+        }
+    },
+    computed: {
+        totalPrice() {
+            return this.price * this.quantity;
+        },
+        imageSrc() {
+            return `http://1.15.220.157:8088/requests/image/${this.imagePath}`
+        }
+    },
+    template: `
+    <li>
+        <img :src="imageSrc" alt="Images">
+        <h3>{{name}}</h3>
+        <span>￥{{price}}</span>
+        <span class="quantity-tag">x{{quantity}}</span>
+        <div class="price-tag">￥{{totalPrice}}</div>
+    </li>
+`
+})
+
+let checkoutConfirm = new Vue({
+    el: '#checkout-confirm',
+    data() {
+        return {
+            totalPrice: 0
+        }
+    },
+    computed: {
+        ids: () => {
+            if (getURLVariable('type') === 'single') {
+                return [{
+                    id: getURLVariable('id'),
+                    index: 0
+                }];
+            } else {
+                // TODO
+            }
+        },
+    },
+    methods: {
+        switchToTab: switchToTab,
+        updateTotalPrice() {
+            checkoutConfirm.totalPrice = 0;
+            for (let i = 0; i < checkoutConfirm.ids.length; ++i) {
+                checkoutConfirm.totalPrice += checkoutConfirm.$refs[i][0].totalPrice;
+            }
+        }
+    }
+})
+
+async function handleCheckoutItem(checkoutItem) {
+    let orderData = {
+        buyer: $.cookie("id"),
+        token: $.cookie("token"),
+
+        receiverName: checkoutForm.form.receiverName,
+        phoneNumber: checkoutForm.form.phoneNumber,
+        campus: checkoutForm.form.campus,
+        dorm: checkoutForm.form.dorm,
+        detailedAddress: checkoutForm.form.detailedAddress,
+        itemID: checkoutItem.itemId,
+
+        quantity: checkoutItem.quantity
+    }
+    $.ajax({
+        url: `${url}/requests/user/insertOrder`,
+        method: 'post',
+        data: JSON.stringify(orderData),
+        contentType: "application/json;charset=utf-8",
+        success: (responseStr) => {
+            let response = JSON.parse(responseStr);
+            if (response.status === 40200) {
+
+            } else {
+                alert(`${response.message}（状态码：${response.status}）`);
+            }
+        }
+    })
+}
+
+let paidButton = new Vue({
+    el: '#paid-button',
+    methods: {
+        async onPay() {
+            for (let i = 0; i < checkoutConfirm.ids.length; ++i) {
+                await handleCheckoutItem(checkoutConfirm.$refs[i][0]);
+            }
+            alert('交易成功！');
+            window.open("../", "_self");
+        }
+    }
+})
+
 $(() => {
         getUserInfo((response) => {
             checkoutForm.form.phoneNumber = response.data.phoneNumber;
         })
+
     }
 );
--- src/main/resources/static/js/item.js
+++ src/main/resources/static/js/item.js
@@ -2,7 +2,7 @@ let itemApp = new Vue({
     el: "#item-app",
     methods: {
         buy() {
-            window.open(`../checkout?id=${this.item.id}`)
+            window.open(`../checkout?id=${this.item.id}&type=single`)
         }
     },
     data: {
@@ -24,32 +24,20 @@ let itemApp = new Vue({
     },
 });
 
-$(
-    () => {
-        let id = getURLVariable("id");
-        $.ajax({
-            url: `${url}/requests/item/${id}`,
-            method: 'get',
-            contentType: "application/json;charset=utf-8",
-            success: (responseStr) => {
-                let response = JSON.parse(responseStr);
-                if (response.status === 30200) {
-                    itemApp.item = response.data;
-                    let date = new Date(itemApp.item.releaseTime);
-                    itemApp.item.releaseDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-                    if (itemApp.item.introduction === null) {
-                        itemApp.item.introduction = "本物品暂无简介";
-                    }
-                    for (let image of itemApp.item.itemImages) {
-                        itemApp.imageList.push({
-                            name: image,
-                            url: `http://1.15.220.157:8088/requests/image/${image}`
-                        });
-                    }
-                } else {
-                    alert(`${response.message}（状态码：${response.status}）`);
-                }
+$(function () {
+        getItemInfo(getURLVariable("id"), response => {
+            itemApp.item = response.data;
+            let date = new Date(itemApp.item.releaseTime);
+            itemApp.item.releaseDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+            if (itemApp.item.introduction === null) {
+                itemApp.item.introduction = "本物品暂无简介";
             }
-        });
+            for (let image of itemApp.item.itemImages) {
+                itemApp.imageList.push({
+                    name: image,
+                    url: `http://1.15.220.157:8088/requests/image/${image}`
+                });
+            }
+        })
     }
-);
+)
