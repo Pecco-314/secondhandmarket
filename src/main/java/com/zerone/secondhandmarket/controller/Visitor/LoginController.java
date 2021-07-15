6d103ca0cbdaa1f207d953db2967903aba7016c2
jxy <858297426@qq.com>
2021-07-15 14:35:09 +0800

    登陆界面修改

--- src/main/java/com/zerone/secondhandmarket/controller/Visitor/LoginController.java
+++ src/main/java/com/zerone/secondhandmarket/controller/Visitor/LoginController.java
@@ -22,7 +22,10 @@ public class LoginController {
     public String userLogin() {
         return "login";
     }
-
+    @RequestMapping("/log-in")
+    public String userLog_in() {
+        return "log-in";
+    }
     @ResponseBody
     @PostMapping("/requests/login/user")
     public String userLogin(@RequestBody UserLoginMessage data) {
