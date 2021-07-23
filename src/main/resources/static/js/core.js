103dcb80d45293349f407a66512c7cc1bd9731c8
Pecco <pecco@qq.com>
2021-07-23 09:01:27 +0800

    个人中心的订单界面（未完全完成）

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -235,3 +235,21 @@ function elAlert(component, content, title, callback) {
         callback: callback
     });
 }
+
+function getImageOrPlaceholder(path) {
+    if (path === null)
+        return `../img/null2.png`;
+    else
+        return `http://1.15.220.157:8088/requests/image/${path}`;
+}
+
+function delCookie() {
+    let keys = document.cookie.match(/[^ =;]+(?==)/g)
+    if (keys) {
+        for (let i = keys.length; i--;) {
+            document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString() // 清除当前域名下的,例如：m.ratingdog.cn
+            document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString() // 清除当前域名下的，例如 .m.ratingdog.cn
+            document.cookie = keys[i] + '=0;path=/;domain=ratingdog.cn;expires=' + new Date(0).toUTCString() // 清除一级域名下的或指定的，例如 .ratingdog.cn
+        }
+    }
+}
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,14 +1,3 @@
-function delCookie() {
-    var keys = document.cookie.match(/[^ =;]+(?==)/g)
-    if (keys) {
-        for (var i = keys.length; i--;) {
-            document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString() // 清除当前域名下的,例如：m.ratingdog.cn
-            document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString() // 清除当前域名下的，例如 .m.ratingdog.cn
-            document.cookie = keys[i] + '=0;path=/;domain=ratingdog.cn;expires=' + new Date(0).toUTCString() // 清除一级域名下的或指定的，例如 .ratingdog.cn
-        }
-    }
-}
-
 Vue.component('page-header', {
     props: {
         isMainPage: Boolean,
