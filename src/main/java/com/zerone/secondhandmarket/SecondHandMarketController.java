905ce90908a37db53685ee8a7b03c9ed8848388b
lzl <1042173993@qq.com>
2021-07-29 18:00:01 +0800

    购物车订单提交时筛选

--- src/main/java/com/zerone/secondhandmarket/SecondHandMarketController.java
+++ src/main/java/com/zerone/secondhandmarket/SecondHandMarketController.java
@@ -1,8 +1,6 @@
 package com.zerone.secondhandmarket;
 
-import com.zerone.secondhandmarket.service.*;
 import com.zerone.secondhandmarket.tools.Router;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,107 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class SecondHandMarketController {
-    @Autowired
-    UserService userService = new UserService();
-    @Autowired
-    AdminService adminService = new AdminService();
-    @Autowired
-    ItemService itemService = new ItemService();
-    @Autowired
-    CartService cartService = new CartService();
-    @Autowired
-    OrderService orderService = new OrderService();
-    @Autowired
-    TagsService tagService = new TagsService();
-    @Autowired
-    ItemImageService itemImageService = new ItemImageService();
-    @Autowired
-    WishlistService wishlistService = new WishlistService();
-
     @RequestMapping("/")
     public String run(HttpServletRequest request) {
-//        Wishlist wishlist=new Wishlist(100004,88);
-//        wishlistService.insertWishlist(wishlist);
-//        wishlistService.getWishlistByUserId(100004);
-//        ItemFilter itemFilter = new ItemFilter();
-//        itemFilter.setPriceOrdering(Ordering.DESC);
-//        List<Item> items = itemService.getItemByFilter(itemFilter);
-//        for (Item item : items) {
-//            System.out.println(item);
-//        }
-//        String keyword = "哑铃";
-//        List<Item> items1 = itemService.getItemByKeyword(keyword);
-//        items1.retainAll(items);
-//        for (Item item : items1) {
-//            System.out.println(item);
-//        }
-        //tagService.insertTag(42,"全新无损");
-        //  User user = new User(6, "huhu", "123456", "1111111", "111@qq.com", UserHead.HEAD1);
-//        Administrator admin = new Administrator(2, "pecco", "123456");
-//       Item item = new Item(0, 4, "测试", ItemType.DIGITAL, 2, 100, 30,"this is a hat",null,null,null, ItemCheckCondition.TRUE);
-//      int id=itemService.insertItem(item);
-
-        //Item item2 = new Item(1, 3, "c++++primer", ItemType.CLOTHES, 3, 10, 5, Arrays.asList("cs+"), "this is a book", "1.jpg", ItemCheckCondition.UNCHECKED);
-//        ItemFilter filter=new ItemFilter();
-//        filter.setCheckCondition(ItemCheckCondition.TRUE);
-        //  filter.setType(ItemType.DIGITAL);
-        //filter.setKeyWords("a++");
-        //filter.setPriceOrdering(Ordering.DESC);
-        //itemService.insertItem(item);
-        //itemService.insertItem(item2);
-        // userService.deleteUser(3);
-        //itemService.updateItem(item);
-        //  List<Item> items = itemService.getItemByFilter(filter);
-        //   List<Item> items=itemService.getItemList();
-        //for (Item item1 : items)
-        //  System.out.println(item1);
-        //    List<Item> item1s = itemService.getItemListOrderByPrice(Ordering.DESC);
-        //   List<Item> items=itemService.getItemList();
-        //  for (Item item1 : item1s)
-        //    System.out.println(item1);
-        // adminService.insertAdmin(admin);
-        // userService.insertUser(user);
-        //adminService.deleteAdmin(1);
-        //userService.deleteUser(1);
-        //System.out.println(userService.getUserById(3).toString());
-        //System.out.println(adminService.getAdminById(2).toString());
-        //System.out.println(userService.getUserByEmail("111@qq.com").toString());
-        /*List<User> users=userService.getUserList();
-        for(User user1:users)
-            System.out.println(user1);*/
-        //String str=userService.getUserByEmail("123@qq.com").toString();
-        //System.out.println(str);
-        //userService.insertUser(user);
-        //userService.updateUser(user);
-        //adminService.updateAdmin(admin);
-//        ShoppingCart cart1=new ShoppingCart(1,2,1);
-//        ShoppingCart cart2=new ShoppingCart(1,1,1);
-//        ShoppingCart cart3=new ShoppingCart(1,3,2);
-//        ShoppingCart cart4=new ShoppingCart(2,3,2);
-        //  cartService.insertCart(cart1);
-        // cartService.insertCart(cart2);
-        //cartService.insertCart(cart3);
-        //cartService.insertCart(cart4);
-        //  cartService.deleteCart(1,2);
-        //   cart4.setQuantity(5);
-        //cartService.modifyItemQuantity(cart4);
-//        List<ShoppingCart> carts = cartService.getCartListByUserId(1);
-//
-//        for (ShoppingCart cart0 : carts)
-//          System.out.println(cart1);
-//        Order order1 = new Order(0, 1, 2, 2, 1, "2021-07-03 20:48:00");
-//        Order order2 = new Order(0, 1, 2, 3, 1, "2021-07-13 20:48:00");
-//        Order order3 = new Order(0, 1, 5, 4, 1, "2021-06-03 20:48:00");
-//        orderService.insertOrder(order1);
-//        orderService.insertOrder(order2);
-//        orderService.insertOrder(order3);
-        //  orderService.deleteOrder(3);
-//        Cart cart = new Cart(1, 73, 2);
-//        cartService.insertOrUpdateCart(cart);
-//        List<Order> orders = orderService.getOrderByUserId(1);
-//        for (Order cart0 : orders)
-//            System.out.println(cart0);
-//        System.out.println(CodeProcessor.decode("4e5c4f484d55"));
         String res = Router.routerForUserAndVistor(request, "index");
         System.out.println(res);
         return res;
